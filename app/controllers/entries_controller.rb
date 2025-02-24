2d443215af933560da5d4c4218d3e1904d5792ae
David Gordon <david.gordon@kellogg.northwestern.edu>
2025-02-24 00:12:03 +0000

    commit 1

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,22 @@
+class EntriesController < ApplicationController
+
+  def new
+    @place = Place.find_by({ "id" => params["place_id"] })
+    @entry = Entry.new
+  end
+
+  def create
+    @place = Place.find_by({ "id" => params["id"] })
+      @entry = Entry.new
+      @entry["title"] = params["title"]
+      @entry["description"] = params["description"]
+      @entry["posted_on"] = params["posted_on"]
+      @entry["place_id"] =params["place.id"]
+
+    @entry.save
+      redirect_to("/places/#{@place.id}")
+    
+    end
+  end
+end
+  
---
+++ app/controllers/travel_log_controller.rb
@@ -0,0 +1,7 @@
+class TravelLogController < ApplicationController
+
+  def index
+    redirect_to("/places")
+  end
+
+end
